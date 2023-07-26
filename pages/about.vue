80967a292eea91065cdd254ed69f62607b4e00cb
jiannei <longjian.huang@foxmail.com>
2023-07-26 13:39:20 +0800

    test: nuxt

---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <section>
+      <p>This page will be displayed at the /about route.</p>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
